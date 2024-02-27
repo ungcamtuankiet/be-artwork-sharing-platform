a02e1c4db5a791b69e2548bc0ad3c1f465bcdd29
ungcamtuankiet <ungcamtuankiet94@gmai.com>
2024-02-27 13:17:55 +0700

    add search by user name

--- be-artwork-sharing-platform/obj/Debug/net7.0/be-artwork-sharing-platform.AssemblyInfo.cs
+++ be-artwork-sharing-platform/obj/Debug/net7.0/be-artwork-sharing-platform.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("be-artwork-sharing-platform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+062d7f8f7389e98554d906f7d675a4d871dee064")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c981c380f1e8ed960d9c17399e1c7b6bbf16227")]
 [assembly: System.Reflection.AssemblyProductAttribute("be-artwork-sharing-platform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("be-artwork-sharing-platform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
