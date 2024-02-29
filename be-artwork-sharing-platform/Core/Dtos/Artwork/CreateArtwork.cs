28f4a09fa225d038b66cd9c2d47333460b8198a0
ungcamtuankiet <ungcamtuankiet94@gmai.com>
2024-02-29 21:42:44 +0700

    fix-error

--- be-artwork-sharing-platform/Core/Dtos/Artwork/CreateArtwork.cs
+++ be-artwork-sharing-platform/Core/Dtos/Artwork/CreateArtwork.cs
@@ -1,7 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace be_artwork_sharing_platform.Core.Dtos.Artwork
+﻿namespace be_artwork_sharing_platform.Core.Dtos.Artwork
 {
     public class CreateArtwork
     {
@@ -10,6 +7,5 @@ namespace be_artwork_sharing_platform.Core.Dtos.Artwork
         public string? Description { get; set; }
         public double Price { get; set; }
         public string Url_Image { get; set; }
-        
     }
 }
--- be-artwork-sharing-platform/obj/Debug/net7.0/be-artwork-sharing-platform.AssemblyInfo.cs
+++ be-artwork-sharing-platform/obj/Debug/net7.0/be-artwork-sharing-platform.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("be-artwork-sharing-platform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94da95fbbe91ab74c96a0c1551479dc06d0cd846")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07bc32f21b7e1727e495c97a3f103f21245b1372")]
 [assembly: System.Reflection.AssemblyProductAttribute("be-artwork-sharing-platform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("be-artwork-sharing-platform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
