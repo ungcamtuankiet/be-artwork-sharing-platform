2fcda6a4d584494650e5ff45d6810f3d8d527d57
ungcamtuankiet <ungcamtuankiet94@gmai.com>
2024-03-09 12:21:01 +0700

    update function user

--- be-artwork-sharing-platform/Core/Dtos/Auth/UserInfoResult.cs
+++ be-artwork-sharing-platform/Core/Dtos/Auth/UserInfoResult.cs
@@ -6,6 +6,8 @@
         public string FullName { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
         public DateTime CreatedAt { get; set; }
         public IEnumerable<string> Roles { get; set; }
     }
